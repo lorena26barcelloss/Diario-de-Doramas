8fa1aa8a9b0b7dec6d8d7fc3b4c429e13a6dbe48
lorena26barcelloss <lorena.souza5@fatec.sp.gov.br>
2025-11-03 22:23:10 -0300

    Inicialização Spring Boot

---
+++ projetodiario/projetodiario/src/main/java/com/diario/projetodiario/ProjetodiarioApplication.java
@@ -0,0 +1,13 @@
+package com.diario.projetodiario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodiarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodiarioApplication.class, args);
+	}
+
+}
