841701a2ae32c5b3ea67bd5ebdf547d7d7d22134
lorena26barcelloss <lorena.souza5@fatec.sp.gov.br>
2025-11-21 23:46:07 -0300

    Atualizando para banco de dados em nuvem

--- projetodiario/projetodiario/src/main/java/com/diario/projetodiario/repository/UserRepository.java
+++ projetodiario/projetodiario/src/main/java/com/diario/projetodiario/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.diario.projetodiario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.diario.projetodiario.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
